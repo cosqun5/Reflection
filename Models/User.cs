74c276dc403c184d8e9c015c15742792b91968c3
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-04-25 11:53:53 +0400

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Task.Models
+{
+    public class User
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private static int age { get; set; }
+        public void GetFullName()
+        {
+            Console.WriteLine(Name +" "+Surname);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Reflection_Task.Models;
+using System.Reflection;
+
+
+Type type = typeof(User);
+
+User user = new User();
+PropertyInfo propId = type.GetProperty("Id",BindingFlags.Instance | BindingFlags.NonPublic);
+propId.SetValue(user, 5);
+Console.WriteLine(propId.GetValue(user));
+
+PropertyInfo propName = type.GetProperty("Name", BindingFlags.Instance | BindingFlags.NonPublic);
+propName.SetValue(user, "Cosqun");
+PropertyInfo propSurname = type.GetProperty("Surname", BindingFlags.Instance | BindingFlags.NonPublic);
+propSurname.SetValue(user, "Salahov");
+
+MethodInfo method = type.GetMethod("GetFullName");
+method.Invoke(user, null);
+
+PropertyInfo propAge = type.GetProperty("age", BindingFlags.Static | BindingFlags.NonPublic);
+propAge.SetValue(null, 21);
+Console.WriteLine(propAge.GetValue(null));
+
+
+//PropertyInfo propAge = type.GetProperty("age",BindingFlags.Static| BindingFlags.NonPublic);
+//MethodInfo metod1 = type.GetMethod("ChangeAge");
+//metod1.Invoke(user,propAge);
+//static void ChangeAge(PropertyInfo propAge)
+//{
+//    propAge.SetValue(null, 21);
+//   Console.WriteLine(propAge.GetValue(null));
+//}
+
+
+
